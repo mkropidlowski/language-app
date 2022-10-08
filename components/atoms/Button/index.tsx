2c8d666960f8087b57e93f781aeefda8ea7f11f6
Michał Kropidłowski <michal@MacBook-Air-Micha.local>
2022-10-08 18:12:10 +0200

    LAN-2: Add button components

---
+++ components/atoms/Button/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react"
+
+
+const Button: FC = (
+    (
+        {
+            // propsy
+        }
+    ) => (
+        <button>
+
+        </button>
+    )
+)
+   
+
+
+export default Button
