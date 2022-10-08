0bb4aafb6259ae61e2ea218f8bb16629a19d5d5d
Michał Kropidłowski <michal@MacBook-Air-Micha.local>
2022-10-08 18:02:08 +0200

    LAN-1: Full setup

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Language App</title>
+      </Head>
+      
+     
+    
+    </div>
+  )
+}
+
+export default Home
