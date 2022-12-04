5b31962896a1652dbba7e7074022e8733c173200
Michał Kropidłowski <michal@MacBook-Air-Micha.local>
2022-12-04 12:48:16 +0100

    LAN-9: change module name

--- components/organism/testbranch/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, HTMLProps } from "react";
-import Heading from "../../atoms/Heading";
-import style from "./UserReview.module.scss";
-
-export interface Props {}
-
-const UserReview: FC<Props & HTMLProps<HTMLDivElement>> = () => (
-    <>to jest praca na LAN-7 ale przenoszę to na LAN-8 i commituje</>
-);
