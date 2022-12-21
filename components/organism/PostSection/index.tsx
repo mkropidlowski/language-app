19b1a599786f5dcb38ab72f55461ced37741baa9
Michał Kropidłowski <michal@MacBook-Air-Micha.local>
2022-12-21 12:34:31 +0100

    change imports

--- components/organism/PostSection/index.tsx
+++ components/organism/PostSection/index.tsx
@@ -1,12 +1,12 @@
-import Post from "components/molecules/Post";
-import style from "./postSection.module.scss";
-import { FC, useState } from "react";
-import { publicEnvs } from "config/envs";
 import axios from "axios";
+import { publicEnvs } from "config/envs";
+import { FC, useState } from "react";
 import { errorTextForDataCouldntFetch } from "config/errorsText/data";
-import ErrorPage from "../ErrorPage";
-import SectionLayout from "components/molecules/SectionLayout";
 import { postSectionHeader } from "config/panelSection/data";
+import SectionLayout from "components/molecules/SectionLayout";
+import Post from "components/molecules/Post";
+import ErrorPage from "../ErrorPage";
+import style from "./postSection.module.scss";
 
 const API = `${publicEnvs.BASE_URL_API}/posts`;
 
